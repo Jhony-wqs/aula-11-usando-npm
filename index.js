2f0c06dfcb4e52f21d3c37350b9a7a0aa89037fd
Jhony Wuaanderson <jhony270288@gmail.com>
2024-05-02 10:25:32 -0300

    exercício

---
+++ index.js
@@ -0,0 +1,6 @@
+const lodash = require("lodash")
+
+
+
+console.log(lodash.isArray([]))// usando ferramentas do modulo para saber se e um array.
+console.log(lodash.kebabCase("Jhony wuanderson de queiroz silva"))//usado a ferramena do modulo para deixar o nome no formato kababCase.
